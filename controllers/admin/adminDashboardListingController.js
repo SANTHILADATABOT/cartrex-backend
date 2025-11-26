800e8c927762c9d911e5b8d72be52eab71fc5edf
Santhiladharshini <dharshini@santhila.co>
2025-11-26 10:31:10 +0530

    kaviya code merge

--- controllers/admin/adminDashboardListingController.js
+++ controllers/admin/adminDashboardListingController.js
@@ -69,31 +69,8 @@ exports.getdashboardcounts = async (req, res) => {
 
   
     const totalBookings = await Booking.countDocuments({ deletstatus: 0 });
-
-    //  const carrierMonthlyData = await Carrier.aggregate([
-    //   {
-    //     $match: {
-    //       status: "active",     // only active
-    //       deletstatus: 0        // not deleted
-    //     }
-    //   },
-    //   {
-    //     $group: {
-    //       _id: { month: { $month: "$createdAt" } },
-    //       count: { $sum: 1 }
-    //     }
-    //   },
-    //   { $sort: { "_id.month": 1 } },
-    //   {
-    //     $project: {
-    //       _id: 0,
-    //       month: {
-    //         $arrayElemAt: [monthNames, "$_id.month"]
-    //       },
-    //       count: 1
-    //     }
-    //   }
-    // ]);
+ 
+    
    const shipperAgg = await Shipper.aggregate([
       { $match: { deletstatus: 0 ,status: "active" , $expr: { $eq: [ { $year: "$createdAt" }, 2025 ] }  } },
       { $group: { _id: { month: { $month: "$createdAt" } }, count: { $sum: 1 } } },
@@ -124,7 +101,48 @@ exports.getdashboardcounts = async (req, res) => {
         carriers: carrierMap[m] || 0
       });
     }
+        // Current Year Range
+    const startCurrent = new Date(now.getFullYear(), 0, 1);
+    const endCurrent = new Date(now.getFullYear(), 11, 31, 23, 59, 59);
 
+    // Last Year Range
+    const lastYear = now.getFullYear() - 1;
+    const startLast = new Date(lastYear, 0, 1);
+    const endLast = new Date(lastYear, 11, 31, 23, 59, 59);
+
+    // Function to calculate total
+    async function getTotal(startDate, endDate) {
+      const result = await Booking.aggregate([
+        {
+          $match: {
+            deletstatus: 0,
+            createdAt: { $gte: startDate, $lte: endDate }
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            totalPrice: { $sum: { $toDouble: "$bookValuetaxinc.total" } }
+          }
+        }
+      ]);
+
+      return result.length ? result[0].totalPrice : 0;
+    }
+
+    // Get totals
+    const currentYearTotal = await getTotal(startCurrent, endCurrent);
+    const lastYearTotal = await getTotal(startLast, endLast);
+
+    // Percentage calculation
+    let percentageChange = 0;
+
+    if (lastYearTotal > 0) {
+      percentageChange = ((currentYearTotal - lastYearTotal) / lastYearTotal) * 100;
+    }
+
+    // Format to 2 decimals
+    percentageChange = Number(percentageChange.toFixed(2));
 
     return res.status(200).json({
       success: true,
@@ -134,7 +152,8 @@ exports.getdashboardcounts = async (req, res) => {
       activeShippers,
       totalTrucks,
       totalBookings},
-         monthly_data: monthlyData
+      Totalearning:{currentYearTotal,percentageChange},
+      monthly_data: monthlyData
     });
 
   } catch (error) {
