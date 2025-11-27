71ccec7184e5f79e968b36292bdb5df07ac332a0
LogeshwariSundaram <logeshwari@santhila.co>
2025-11-27 10:38:37 +0530

    new brach created

--- routes/Api/ApiRoutes.js
+++ routes/Api/ApiRoutes.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 const multer = require("multer");
 const path = require("path");
 const router = express.Router();
-const {signup,login,logout,verifyOtp,sendOtp,UserVerification,forgotPassword,resetPassword,signUpVerification} = require('../../controllers/api/authController');
-const {createSpace,searchSpaces,getSpaces,updateSpace,deleteSpace,getspacedetails,getSpaceResult,addSpacesDetails,getSpacesByCarrierUserId,editSpacesDetails} = require('../../controllers/api/spaceController');
-const {createOrUpdateCarrierProfile} = require('../../controllers/api/carrierController');
-const {createOrUpdateshipperProfile} = require('../../controllers/api/shipperController');
+const { signup, login, logout, verifyOtp, sendOtp, UserVerification, forgotPassword, resetPassword } = require('../../controllers/api/authController');
+const { createSpace, searchSpaces, getSpaces, updateSpace, deleteSpace, getspacedetails, getSpaceResult, addSpacesDetails, getSpacesByCarrierUserId, editSpacesDetails } = require('../../controllers/api/spaceController');
+const { createupdateProfile, createupdateProfilePhoto } = require('../../controllers/api/carrierController');
+const { createOrUpdateshipperProfile } = require('../../controllers/api/shipperController');
+const { createTruckProfile, uploadTruckPhotos, createTruckRoute } = require('../../controllers/api/TruckController');
 const upload = multer({ storage: multer.memoryStorage() });
 //auth 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post('/signUpVerification', signUpVerification);
 router.post('/verify-otp', verifyOtp);
 router.post('/sendOtp', sendOtp);
 router.post('/userVerificaion', UserVerification);
@@ -21,11 +21,31 @@ router.post('/logout', logout);
 
 //carrier 
 
-router.post('/carrierprofile', upload.single('photo'), createOrUpdateCarrierProfile);
+//router.post('/carrierprofile', upload.single('photo'), createOrUpdateCarrierProfile);
+router.post('/createupdateProfile', createupdateProfile);
+
+router.post('/createupdateProfilePhoto', upload.single('photo'), createupdateProfilePhoto);
 
 //shipper
 
-router.post('/shipperprofile',upload.single('photo'), createOrUpdateshipperProfile);
+router.post('/shipperprofile', upload.single('photo'), createOrUpdateshipperProfile);
+
+//truck
+
+router.post("/createtruckprofile",upload.fields([
+        { name: "insurance", maxCount: 1 }
+    ]),  createTruckProfile);
+
+router.post("/uploadtruckphotos",upload.fields([
+        { name: "truckProfile", maxCount: 1 },
+        { name: "coverPhoto", maxCount: 1 },
+        { name: "photos", maxCount: 6 }
+    ]),
+    uploadTruckPhotos
+);
+
+router.post("/createtruckroute", createTruckRoute);
+
 //space
 
 router.get('/getSpaceResult', getSpaceResult);
