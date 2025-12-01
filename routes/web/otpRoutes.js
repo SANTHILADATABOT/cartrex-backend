4bf5610ecdb0d4b4db4ed9c1110f23692237f7fe
Santhiladharshini <dharshini@santhila.co>
2025-12-01 18:57:38 +0530

    bug fix

--- routes/web/otpRoutes.js
+++ routes/web/otpRoutes.js
@@ -4,8 +4,11 @@ const { sendOtp, verifyOtp } = require("../../controllers/otpController");
 
 // SEND OTP
 router.post("/send", sendOtp);
+// SEND OTP
+router.post("/send-otp", sendOtp);
 
 // VERIFY OTP
 router.post("/verify", verifyOtp);
+router.post("/verify-otp", verifyOtp);
 
 module.exports = router;
