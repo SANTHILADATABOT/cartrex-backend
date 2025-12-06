77ac96260b64fe7734de1caaf0e128a1f8aaad45
Vennila2001 <vennila@santhila.co>
2025-12-06 10:35:05 +0530

    pull

---
+++ controllers/admin/GeneralSettingsController.js
@@ -0,0 +1,25 @@
+const Policy = require("../../models/Policy");
+
+// CREATE or UPDATE a policy
+exports.saveGeneral = async (req, res) => {
+  try {
+    const { type, content } = req.body;
+
+    const policy = await Policy.findOne({   type  });
+
+    if (policy) {
+      policy.content = content;
+      await policy.save();
+      return res.json({   success: true,message: `${type} updated successfully`, policy });
+    }
+
+    const newPolicy = await Policy.create({ type, content });
+    res.status(200).json({ message: `${type} created successfully`, policy: newPolicy });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+
